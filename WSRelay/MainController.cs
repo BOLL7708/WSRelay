25388d6ad7028cd8d7783021b1759bd9c493d70b
Andreas Aronsson <BOLL@7708.net>
2022-10-29 13:02:20 +0200

    Initial commit
    Working server, GUI, channel support

---
+++ WSRelay/MainController.cs
@@ -0,0 +1,63 @@
+﻿using BOLL7708;
+using SuperSocket.WebSocket.Server;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BOLL7708.SuperServer;
+
+namespace WSRelay
+{
+    internal class MainController
+    {
+        SuperServer _server = new SuperServer();
+        ConcurrentDictionary<string, string> _channels = new();
+
+        public Action<ServerStatus, int> StatusAction = (status, count) => { };
+
+        public MainController() {
+            _server.StatusAction += (status, count) =>
+            {
+                Debug.WriteLine(Enum.GetName(typeof(ServerStatus), status));
+                StatusAction.Invoke(status, count);
+
+            };
+            _server.StatusMessageAction += (WebSocketSession? session, bool status, string message) => {
+                if(!status) _channels.TryRemove(session?.SessionID ?? "", out string? oldSessionID);
+            };
+            _server.MessageReceivedAction += (WebSocketSession session, string message) =>
+            {
+                var hasChannel = _channels.TryGetValue(session.SessionID, out string? channel);
+                if(!hasChannel && message.StartsWith("CHANNEL:"))
+                {
+                    var messageArr = message.Split(':');
+                    if (messageArr.Length == 2) channel = messageArr[1].Trim();
+                    if(channel != null && channel.Length > 0) _channels[session.SessionID] = channel;
+                }
+                Debug.WriteLine(message);
+                if(channel != null && channel.Length > 0)
+                {
+                    List<string> sessionIDs = new();
+                    foreach(string key in _channels.Keys)
+                    {
+                        if (_channels[key] == channel && key != session.SessionID)
+                        {
+                            sessionIDs.Add(key);
+                        }
+                    }
+                    _server.SendMessageToGroup(sessionIDs.ToArray(), message);
+                } else
+                {
+                    _server.SendMessage(session, "ERROR: Not connected to any channel");
+                }
+            };
+        }
+
+        public void StartOrRestartServer(int port) {
+            _server.Start(port);
+        }
+    }
+}
---
+++ WSRelay/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BOLL7708;
+
+namespace WSRelay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainController _controller = new MainController();
+        private Properties.Settings _settings = Properties.Settings.Default;
+        private ConcurrentDictionary<string, int> _stateDic = new ConcurrentDictionary<string, int>();
+
+        const string 
+            KEY_SECOND_SESSION = "second_session",
+            KEY_SECOND_IN = "second_in",
+            KEY_SECOND_OUT = "second_out",
+            KEY_SESSION_COUNT = "session_count",
+            KEY_MESSAGES_IN_TOTAL = "messages_in_total",
+            KEY_MESSAGES_OUT_TOTAL = "messages_in_total";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowUtils.CheckIfAlreadyRunning("WSRelay");
+            // WindowUtils.Minimize(this, false); // TODO: Use this when we actually have a tray icon.
+
+            UpdatePortLabel();
+
+            _stateDic.TryAdd(KEY_SECOND_SESSION, 0);
+            _stateDic.TryAdd(KEY_SESSION_COUNT, 0);
+            _stateDic.TryAdd(KEY_MESSAGES_IN_TOTAL, 0);
+            _stateDic.TryAdd(KEY_MESSAGES_OUT_TOTAL, 0);
+
+            _controller.StatusAction += (status, count) =>
+            {
+                try
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        var now = DateTime.Now;
+                        var shouldUpdateStatisticsText = false;
+                        switch (status)
+                        {
+                            case SuperServer.ServerStatus.Connected:
+                                Label_Status.Background = Brushes.OliveDrab;
+                                Label_Status.Content = "Connected";
+                                break;
+                            case SuperServer.ServerStatus.Disconnected:
+                                Label_Status.Background = Brushes.Tomato;
+                                Label_Status.Content = "Disconnected";
+                                break;
+                            case SuperServer.ServerStatus.Error:
+                                Label_Status.Background = Brushes.Gray;
+                                Label_Status.Content = "Error";
+                                break;
+                            case SuperServer.ServerStatus.DeliveredCount:
+                                {
+                                    _stateDic.TryGetValue(KEY_SECOND_OUT, out int second);
+                                    if (now.Second != second)
+                                    {
+                                        _stateDic.TryUpdate(KEY_SECOND_OUT, now.Second, second);
+                                        UpdateValueInDic(KEY_MESSAGES_OUT_TOTAL, count);
+                                        shouldUpdateStatisticsText = true;
+                                    }
+                                }
+                                break;
+                            case SuperServer.ServerStatus.ReceivedCount:
+                                {
+                                    _stateDic.TryGetValue(KEY_SECOND_IN, out int second);
+                                    if (now.Second != second)
+                                    {
+                                        _stateDic.TryUpdate(KEY_SECOND_IN, now.Second, second);
+                                        UpdateValueInDic(KEY_MESSAGES_IN_TOTAL, count);
+                                        shouldUpdateStatisticsText = true;
+                                    }
+                                }
+                                break;
+                            case SuperServer.ServerStatus.SessionCount:
+                                {
+                                    _stateDic.TryGetValue(KEY_SECOND_SESSION, out int second);
+                                    if (now.Second != second)
+                                    {
+                                        _stateDic.TryUpdate(KEY_SECOND_SESSION, now.Second, second);
+                                        UpdateValueInDic(KEY_SESSION_COUNT, count);
+                                        shouldUpdateStatisticsText = true;
+                                    }
+                                }
+                                break;
+                        }
+                        if(shouldUpdateStatisticsText)
+                        {
+                            UpdateStatistics();
+                        }
+                    });
+                }
+                catch (TaskCanceledException e)
+                {
+                    Debug.WriteLine($"Caught exception: {e.Message}");
+                }
+            };
+
+            UpdateStatistics();
+            _controller.StartOrRestartServer(_settings.Port);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SingleInputDialog(this, _settings.Port.ToString(), "Port");
+            dlg.ShowDialog();
+            var result = dlg.DialogResult == true ? dlg.value : null;
+            if(result != null)
+            {
+                var port = int.Parse(result);
+                _controller.StartOrRestartServer(port);
+                _settings.Port = port;
+                _settings.Save();
+                UpdatePortLabel();
+            }
+            
+        }
+
+        private void UpdateStatistics() {
+            _stateDic.TryGetValue(KEY_SESSION_COUNT, out int sessionCount);
+            _stateDic.TryGetValue(KEY_MESSAGES_IN_TOTAL, out int inCount);
+            _stateDic.TryGetValue(KEY_MESSAGES_OUT_TOTAL, out int outCount);
+            Label_Stats.Content = $"Sessions: {sessionCount}\nMessages in: {inCount}\nMessages out: {outCount}\nVersion: {Properties.Resources.Version}";
+        }
+
+        private void UpdatePortLabel() {
+            Label_Port.Content = "Port: "+ _settings.Port;
+        }
+
+        private bool UpdateValueInDic(string key, int value)
+        {
+            var ok = _stateDic.TryGetValue(key, out int oldValue);
+            return ok && _stateDic.TryUpdate(key, value, oldValue);
+
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            WindowUtils.OnStateChange(this, false);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            WindowUtils.DestroyTrayIcon();
+        }
+    }
+}
